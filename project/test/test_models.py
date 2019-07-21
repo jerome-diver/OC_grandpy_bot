2ba6ecd2cc6619bd2658b2d6dd2fc310aa3d1b42
Jérôme Lanteri <jerome.archlinux@gmail.com>
2019-07-21 12:25:18 +0700

    add test for db global scope variable of project/models.py Convert Content test to class TestContent

--- project/test/test_models.py
+++ project/test/test_models.py
@@ -6,12 +6,16 @@
     sqlite database
 """
 
-def test_content():
-    """TEst of class Content"""
-
+class TestContent:
+    """Test class Content"""
     from project.models import Content
     content = Content("test")
-    assert content.word == "test"
+
+    def test_init(self):
+        """Test of initialization of Content object instance"""
+
+        assert self.content.word == "test"
+
 
 def test_init_stop_words(capsys):
     """Test of function init_stop_words"""
@@ -21,3 +25,11 @@ def test_init_stop_words(capsys):
     captured = capsys.readouterr()
     assert captured.out == "INFO :: Database with stop words content " \
                                 "initialized\n"
+
+def test_db(capsys):
+    """Test type of global module scope  variable db"""
+
+    from project.models import db
+    print(type(db))
+    captured = capsys.readouterr()
+    assert  captured.out == "<class 'flask_sqlalchemy.SQLAlchemy'>\n"
