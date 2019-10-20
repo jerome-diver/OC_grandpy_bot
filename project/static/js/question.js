f503c6fdf03e578ad025d266f0bdc1f5bc3c8308
Jérôme Lanteri <jerome.archlinux@gmail.com>
2019-10-20 18:55:20 +0700

    wash question.js

--- project/static/js/question.js
+++ project/static/js/question.js
@@ -161,30 +161,6 @@ class GoogleMap {
 }
 
 $(document).ready( () => {
-  //function getBrowserLocation() {
-  //  if (navigator.geolocation) {
-  //    navigator.geolocation.getCurrentPosition(
-  //      function(position) {
-  //        var pos = {
-  //          lat: position.coords.latitude,
-  //          lng: position.coords.longitude
-  //        };
-
-   //     }, function() {  });
-   //  } else {
-   //    // Browser doesn't support Geolocation
-   //    //handleLocationError(false, infoWindow, map.getCenter());
-   //  }
-  //}
-
-  //function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  //  infoWindow.setPosition(pos);
-  //  infoWindow.setContent(browserHasGeolocation ?
-  //                        'Error: The Geolocation service failed.' :
-  //                        'Error: Your browser doesn\'t support geolocation
-  //.');
-  //  infoWindow.open(map);
-  //}
   const user = new User();
   const bot = new Bot();
   const dialog = new Dialog(user, bot);
